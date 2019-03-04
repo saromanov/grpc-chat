aa7320debfe629e16a3311764ef00dc8a4faa8cd
Sergey <xxsmotur@gmail.com>
2019-03-04 23:42:44 +0500

    add config for chat service

---
+++ app/service/config/config.go
@@ -0,0 +1,7 @@
+package config
+
+// Config defines configuration for service
+type Config struct {
+	DBName     string
+	DBPassword string
+}
--- app/service/service.go
+++ app/service/service.go
@@ -1,2 +1,9 @@
 // Package service provides initialization of the chat service
 package service
+
+import "github.com/saromanov/grpc-chat/app/service/config"
+
+// New provides initialization of the all components of the service
+func New(conf *config.Config) error {
+	return nil
+}
