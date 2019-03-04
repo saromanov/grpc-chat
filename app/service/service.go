89991ca2723cebf2a0a6d2ab1f51d9ddc072700d
Sergey <xxsmotur@gmail.com>
2019-03-05 00:18:46 +0500

    init of grpc server

--- app/service/service.go
+++ app/service/service.go
@@ -2,6 +2,8 @@
 package service
 
 import (
+	"net"
+	"google.golang.org/grpc"
 	"github.com/pkg/errors"
 	"github.com/saromanov/grpc-chat/app/service/chat/handlers"
 	"github.com/saromanov/grpc-chat/app/service/chat/storage"
@@ -9,12 +11,14 @@ import (
 )
 
 // New provides initialization of the all components of the service
-func New(conf *config.Config) error {
+func New(conf *config.Config, lis net.Listener) error {
 	st, err := storage.New(conf)
 	if err != nil {
 		return errors.Wrap(err, "unable to init storage")
 	}
-	err = handlers.New()
+
+	grpcServer := grpc.NewServer()
+	err = handlers.New(grpcServer,nil)
 	if err != nil {
 		return errors.Wrap(err, "unable to init handler")
 	}
