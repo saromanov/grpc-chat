ca92b82bc7eeeaf551b0368410c8dcf5e0c737b3
Sergey <xxsmotur@gmail.com>
2019-03-05 00:29:58 +0500

    creating grpc server with channels

--- app/service/service.go
+++ app/service/service.go
@@ -3,24 +3,30 @@ package service
 
 import (
 	"net"
-	"google.golang.org/grpc"
+
 	"github.com/pkg/errors"
 	"github.com/saromanov/grpc-chat/app/service/chat/handlers"
 	"github.com/saromanov/grpc-chat/app/service/chat/storage"
 	"github.com/saromanov/grpc-chat/app/service/config"
+	"google.golang.org/grpc"
 )
 
 // New provides initialization of the all components of the service
-func New(conf *config.Config, lis net.Listener) error {
+func New(conf *config.Config, lis net.Listener) (chan bool, error) {
 	st, err := storage.New(conf)
 	if err != nil {
-		return errors.Wrap(err, "unable to init storage")
+		return nil, errors.Wrap(err, "unable to init storage")
 	}
 
 	grpcServer := grpc.NewServer()
-	err = handlers.New(grpcServer,nil)
+	err = handlers.New(grpcServer, nil)
 	if err != nil {
-		return errors.Wrap(err, "unable to init handler")
+		return nil, errors.Wrap(err, "unable to init handler")
 	}
-	return nil
+	done := make(chan bool)
+	go func(l net.Listener) {
+		grpcServer.Serve(lis)
+		done <- true
+	}(lis)
+	return done, nil
 }
