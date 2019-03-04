59be6a9333978e211af3fc7fca37521dc1534796
Sergey <xxsmotur@gmail.com>
2019-03-05 00:25:10 +0500

    parsing of the config file

--- app/main.go
+++ app/main.go
@@ -1,6 +1,29 @@
 package main
 
-import "net"
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+
+	"github.com/saromanov/grpc-chat/app/service"
+	"github.com/saromanov/grpc-chat/app/service/config"
+	"gopkg.in/yaml.v2"
+)
+
+// parseConfig provides parsing of the config .yml file
+func parseConfig(path string) (*config.Config, error) {
+	yamlFile, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("unable to open config file: %v", err)
+	}
+	var c *config.Config
+	err = yaml.Unmarshal(yamlFile, &c)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse .config.yml: %v", err)
+	}
+
+	return c, nil
+}
 
 func main() {
 	lis, err := net.Listen("tcp", ":14200")
@@ -8,4 +31,9 @@ func main() {
 		panic("unable to listen: %v")
 	}
 	defer lis.Close()
+
+	err = service.New(nil, lis)
+	if err != nil {
+		panic(err)
+	}
 }
