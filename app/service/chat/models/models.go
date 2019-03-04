d90a0fb1b9194efa88474a581e0855053f040995
Sergey <xxsmotur@gmail.com>
2019-03-04 18:45:16 +0500

    add message struct

--- app/service/chat/models/models.go
+++ app/service/chat/models/models.go
@@ -1,7 +1,20 @@
 // package models contains models for chat
 package models
 
+import "github.com/saromanov/grpc-chat/proto/chat"
+
 // Message defines struct for messaging
 type Message struct {
+	ID string
+	Body string
+	User string
+}
 
+// ToProto provides converting of the message to
+// protobuf representation
+func (m *Message) ToProto()*chat.Message {
+	return &chat.Message {
+		Body: m.Body,
+		User: m.User,
+	}
 }
