560ba9164faf686b13c9a45283eed1a7435cf7eb
Sergey <xxsmotur@gmail.com>
2019-03-07 23:55:07 +0500

    Add SendMessage method

--- app/service/chat/controller/controller.go
+++ app/service/chat/controller/controller.go
@@ -4,14 +4,15 @@ package controller
 
 import (
 	"github.com/pkg/errors"
+	"github.com/saromanov/grpc-chat/app/service/chat/models"
 	"github.com/saromanov/grpc-chat/app/service/chat/storage"
 	"github.com/saromanov/grpc-chat/app/service/config"
+	"github.com/saromanov/grpc-chat/proto/chat"
 )
-
 
 // Controller defines controller for user and storage
 type Controller struct {
-	st *storage.Storage
+	st   *storage.Storage
 	conf *config.Config
 }
 
@@ -21,8 +22,18 @@ func New(conf *config.Config) (*Controller, error) {
 	if err != nil {
 		return nil, errors.Wrap(err, "unable to init controller")
 	}
-	return &Controller{
-		st: st,
-		conf: conf, 
+	return nil, &Controller{
+		st:   st,
+		conf: conf,
 	}
+}
+
+// SendMessage provides inserting of the new message
+func (c *Controller) SendMesage(req *chat.Message) error {
+	m := &models.Message{
+		Body: req.Body,
+		User: req.User,
+	}
+
+	return c.st.Insert(m)
 }
